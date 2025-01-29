cfd2a1e1faaeb56f309441cc6c62695f3bd1cad1
Vrushabh Dave <110376613+Vrushabh2@users.noreply.github.com>
2025-01-29 19:20:14 +0530

    Update RNEncryptedStorageModuleUnitTest.java

--- android/src/androidTest/java/com/emeraldsanto/encryptedstorage/RNEncryptedStorageModuleUnitTest.java
+++ android/src/androidTest/java/com/emeraldsanto/encryptedstorage/RNEncryptedStorageModuleUnitTest.java
@@ -13,77 +13,82 @@ import static org.mockito.Mockito.verify;
 
 @RunWith(AndroidJUnit4.class)
 public class RNEncryptedStorageModuleUnitTest {
-    private RNEncryptedStorageModule module;
+  private RNEncryptedStorageModule module;
 
-    @Before
-    public void setUp() {
-        module = new RNEncryptedStorageModule(new ReactApplicationContext(InstrumentationRegistry.getInstrumentation().getTargetContext()));
-        module.clear(mock(Promise.class));
-    }
+  @Before
+  public void setUp() {
+    ReactApplicationContext reactApplicationContext = mock(ReactApplicationContext.class);
 
-    @Test
-    public void shouldGetAndSet() {
-        Promise promise1 = mock(Promise.class);
-        module.getItem("test", promise1);
-        verify(promise1).resolve(null);
+    module = new RNEncryptedStorageModule(reactApplicationContext);
 
-        Promise promise2 = mock(Promise.class);
-        module.setItem("test", "asd", promise2);
-        verify(promise2).resolve("asd");
+    module.clear(mock(Promise.class));
+  }
 
-        Promise promise3 = mock(Promise.class);
-        module.getItem("test", promise3);
-        verify(promise3).resolve("asd");
-    }
+  @Test
+  public void shouldGetAndSet() {
+    Promise promise1 = mock(Promise.class);
+    module.getItem("test", promise1);
+    verify(promise1).resolve(null);
 
-    @Test
-    public void shouldRemove() {
-        Promise promise1 = mock(Promise.class);
-        module.setItem("test", "asd", promise1);
-        verify(promise1).resolve("asd");
+    Promise promise2 = mock(Promise.class);
+    module.setItem("test", "asd", promise2);
+    verify(promise2).resolve("asd");
 
-        Promise promise2 = mock(Promise.class);
-        module.getItem("test", promise2);
-        verify(promise2).resolve("asd");
+    Promise promise3 = mock(Promise.class);
+    module.getItem("test", promise3);
+    verify(promise3).resolve("asd");
+  }
 
-        Promise promise3 = mock(Promise.class);
-        module.removeItem("test", promise3);
-        verify(promise3).resolve("test");
+  @Test
+  public void shouldRemove() {
+    Promise promise1 = mock(Promise.class);
+    module.setItem("test", "asd", promise1);
+    verify(promise1).resolve("asd");
 
-        Promise promise4 = mock(Promise.class);
-        module.getItem("test", promise4);
-        verify(promise4).resolve(null);
-    }
+    Promise promise2 = mock(Promise.class);
+    module.getItem("test", promise2);
+    verify(promise2).resolve("asd");
 
-    @Test
-    public void shouldClear() {
-        Promise promise1 = mock(Promise.class);
-        module.setItem("test", "asd", promise1);
-        verify(promise1).resolve("asd");
+    Promise promise3 = mock(Promise.class);
+    module.removeItem("test", promise3);
+    verify(promise3).resolve("test");
 
-        Promise promise2 = mock(Promise.class);
-        module.getItem("test", promise2);
-        verify(promise2).resolve("asd");
+    Promise promise4 = mock(Promise.class);
+    module.getItem("test", promise4);
+    verify(promise4).resolve(null);
+  }
 
-        Promise promise3 = mock(Promise.class);
-        module.clear(promise3);
-        verify(promise3).resolve(null);
+  @Test
+  public void shouldClear() {
+    Promise promise1 = mock(Promise.class);
+    module.setItem("test", "asd", promise1);
+    verify(promise1).resolve("asd");
 
-        Promise promise4 = mock(Promise.class);
-        module.getItem("test", promise4);
-        verify(promise4).resolve(null);
-    }
+    Promise promise2 = mock(Promise.class);
+    module.getItem("test", promise2);
+    verify(promise2).resolve("asd");
 
-    @Test
-    public void shouldKeepValuesWhenRecreated() {
-        Promise promise1 = mock(Promise.class);
-        module.setItem("test", "asd", promise1);
-        verify(promise1).resolve("asd");
+    Promise promise3 = mock(Promise.class);
+    module.clear(promise3);
+    verify(promise3).resolve(null);
 
-        module = new RNEncryptedStorageModule(new ReactApplicationContext(InstrumentationRegistry.getInstrumentation().getTargetContext()));
+    Promise promise4 = mock(Promise.class);
+    module.getItem("test", promise4);
+    verify(promise4).resolve(null);
+  }
 
-        Promise promise2 = mock(Promise.class);
-        module.getItem("test", promise2);
-        verify(promise2).resolve("asd");
-    }
+  @Test
+  public void shouldKeepValuesWhenRecreated() {
+    Promise promise1 = mock(Promise.class);
+    module.setItem("test", "asd", promise1);
+    verify(promise1).resolve("asd");
+
+    // Recreate the module with a mocked ReactApplicationContext
+    ReactApplicationContext reactApplicationContext = mock(ReactApplicationContext.class);
+    module = new RNEncryptedStorageModule(reactApplicationContext);
+
+    Promise promise2 = mock(Promise.class);
+    module.getItem("test", promise2);
+    verify(promise2).resolve("asd");
+  }
 }
